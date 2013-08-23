7651347d94acb9fd9606848b7aa6d373936b5b1e
Michael Smith <msmithnova@gmail.com>
2013-08-22 21:00:04 -0300

    Initial commit - Create app, modify gemfile, resolve dependencies

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FortFolly::Application.config.secret_token = 'c84e54f8dadd20537f97cba3ed867a5cce2d3416a686360344adc4afa105b839ec4ba9dff9f7ab03a178eed263c1db541647b92767d25f1e52771bbcd31375b8'
