4b6d951fac45995ece9a184068289e91b95e85a8
Michael Smith <msmithnova@gmail.com>
2013-08-24 05:20:29 -0300

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Fort Folly Website!"
       redirect_to @user
     else
